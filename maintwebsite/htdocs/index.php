b24c557c46a1a3a4abc6896b9b288445527c3c63
Theo Chino <github@theochino.com>
2022-02-21 11:31:30 +0000

    Added the code to get the registration link that is usually sent by email.

--- maintwebsite/htdocs/index.php
+++ maintwebsite/htdocs/index.php
@@ -1,5 +1,21 @@
 <?php
-
-	echo "This is the maintenance scripts modules to perform all the routines.<BR>";
-
+	$mysqli = new mysqli("localhost","usracct","","RepMyBlock");
+	$sql = "SELECT * FROM RepMyBlock.SystemUserEmail ORDER BY SystemUserEmail_Received DESC";
+	
+	// Check connection
+	if ($mysqli -> connect_errno) {
+		echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+		exit();
+	}
+	
+	// Perform query
+	if ($result = $mysqli -> query($sql)) {
+		while($row = mysqli_fetch_row($result)) {
+			echo "<A HREF=\"https://dev-frontend-web.repmyblock.org/ml" . $row[3] . 
+											"/exp/register/intake\" TARGET=\"DEVREP\">" . $row[1] . "</A> - " . 
+											$row[7] . " - Concluded: " . $row[5] . "<BR>\n";
+		}
+		$result -> free_result();
+	}
+	$mysqli -> close();
 ?>

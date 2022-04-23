92cd8bafc7a802aa8c70435b9fcdf31ac0b0d9de
User Account <usracct@devmail.repmyblock.nyc>
2022-04-23 22:33:38 +0000

    New files

---
+++ statlib/pdf/Config/Vars.php
@@ -0,0 +1,16 @@
+<?php
+  // This is the Static Libs that configure variable only that are unique 
+  // to the configuration of this instance.
+
+  $FrontEndPDF = "https://dev-repmyblock-pdf.repmyblock.nyc";
+  $FrontEndStatic = "https://dev-repmyblock-static.repmyblock.nyc";
+  $FrontEndWebsite = "https://dev-repmyblock.repmyblock.nyc";
+  $GoogleMapKeyID = ""; // Google Map api but being deprecated
+  $DatedFilesID = 9;           // Current BOE File
+  $DatedFiles = "20190204";    // Current BOE File in use
+  $FirstDayPetition = 1551139200;
+  $LastDayPetiton = 1554379199;
+  $SignaturesRequired = 0.03;
+  $NYVoterRegistration = "https://voterlookup.elections.ny.gov";
+  $MailChimpAPI = ""; // API to send email automatically
+?>
---
+++ statlib/pdf/DBsLogins/DB_OutragedDems.php
@@ -0,0 +1,14 @@
+<?php
+$databasename = "RepMyBlock";
+$databaseserver = "localhost";
+$databaseuser = "usracct";
+$databasepassword = "";
+$databaseport = "3306";
+
+$DBErrorsFilename = "/tmp/DBErrors_" . $_SERVER["SERVER_ADDR"] . ".txt";
+
+$sslkeys["srvkey"] = "";
+$sslkeys["srvcert"] = "";
+$sslkeys["srvca"] = "";
+
+?>

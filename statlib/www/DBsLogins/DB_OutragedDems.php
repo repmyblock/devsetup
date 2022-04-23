7256dcf426ed3b0f72144b1a0a04c0b38646b26c
Theo Chino <theo@outrageddems.nyc>
2022-04-23 18:56:41 -0400

    Added the WWW static files.

---
+++ statlib/www/DBsLogins/DB_OutragedDems.php
@@ -0,0 +1,14 @@
+<?php
+$databasename = "NYSVoters";
+$databaseserver = "localhost";
+$databaseuser = "usracct";
+$databasepassword = "";
+$databaseport = "3306";
+
+$DBErrorsFilename = "/tmp/DBErrors_" . $_SERVER["SERVER_ADDR"] . ".txt";
+
+$sslkeys["srvkey"] = "";
+$sslkeys["srvcert"] = "";
+$sslkeys["srvca"] = "";
+
+?>

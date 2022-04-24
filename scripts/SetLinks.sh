6ce1c472a2a209b4ed46333a60fd33f7e004c68d
Theo Chino <theo@outrageddems.nyc>
2022-04-24 13:51:44 -0400

    Fixed the path here too.

--- scripts/SetLinks.sh
+++ scripts/SetLinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -r /home/usracct/RepMyBlock/webpages/www/statlib
-ln -s /home/usracct/statlibs/www /home/usracct/RepMyBlock/webpages/www/statlib
-rm -r /home/usracct/RepMyBlock/webpages/pdf/statlib
-ln -s /home/usracct/statlibs/pdf /home/usracct/RepMyBlock/webpages/pdf/statlib
+rm -r /home/usracct/RepMyBlock/website/www/statlib
+ln -s /home/usracct/statlibs/www /home/usracct/RepMyBlock/website/www/statlib
+rm -r /home/usracct/RepMyBlock/website/pdf/statlib
+ln -s /home/usracct/statlibs/pdf /home/usracct/RepMyBlock/website/pdf/statlib

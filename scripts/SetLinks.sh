3475ffa69a9a7e50cd6866ce94ae734580d3b8b5
Theo Chino <theo@outrageddems.nyc>
2022-04-24 22:18:54 -0400

    Changed the directory

--- scripts/SetLinks.sh
+++ scripts/SetLinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -r /home/usracct/RepMyBlock/website/www/statlib
-ln -s /home/usracct/statlibs/www /home/usracct/RepMyBlock/website/www/statlibs
+ln -s /home/usracct/statlib/www /home/usracct/RepMyBlock/website/www/statlibs
 rm -r /home/usracct/RepMyBlock/website/pdf/statlib
-ln -s /home/usracct/statlisb/pdf /home/usracct/RepMyBlock/website/pdf/statlibs
+ln -s /home/usracct/statlib/pdf /home/usracct/RepMyBlock/website/pdf/statlibs
